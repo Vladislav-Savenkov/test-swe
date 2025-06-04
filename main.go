1d9f604d892bd58cc3eddad037145f503a038512
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-06-04 15:16:33 +0500

    Fix Issue#1

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func HelloWorld(name string) string {
-    return fmt.Sprintf("Hi, %s!", name)
+    return fmt.Sprintf("Hello, %s!", name)
 }
 
 func main() {
-    fmt.Println(HelloWorld("World"))
+    fmt.Println()
 }
